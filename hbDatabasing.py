cf1bc88858763a52881701709569b699224d08ff
LiuetenantBob <145494032+LiuetenantBob@users.noreply.github.com>
2023-09-20 22:37:29 +0530

    Add files via upload

--- hbDatabasing.py
+++ hbDatabasing.py
@@ -1,9 +1,15 @@
 from turtle import distance
 import mysql.connector as connector
+import matplotlib.pyplot as plt
+import numpy as np
+
+
 """
 Exceptions:
 Exception for database does not exist - 1049 (42000): Unknown database 'psdatabase'
 Exception for table does not exist - 1146 (42S02): Table 'psdatabase.data' doesn't exist
+
+demand energy is in GigaWatt hours
 
 Format for table:
 Location|Latitude|Longitude|Type of source|Nearest Power Substation|Latitude of SS|Longituse of SS|If active plant exists|If yes power supply capacity else none
@@ -20,7 +26,21 @@ longitudeSS double,
 plantOwner varchar(150),
 plantCapacity double
 );
+
+create table demandData(
+location varchar(150),
+latitude double,
+longitude double,
+powerDemand double,
+growthRate float
+);
+
+create table users(
+emailID varchar(40),
+pass varchar(40)
+);
 """
+
 pwd = "0409"
 
 
@@ -29,7 +49,7 @@ def init_db():
     dbError = None
     try:
         connection = connector.connect(host="localhost", user="root", password=pwd, database="psdatabase")
-        print("Connected")
+        print("Connected to database")
     except Exception as exp:
         dbError = str(exp)
     if dbError == "1049 (42000): Unknown database 'psdatabase'":
@@ -40,6 +60,14 @@ def init_db():
         dbCursor.close()
     if dbError != None and dbError != "1049 (42000): Unknown database 'psdatabase'":
         print("Unsolvable Error Encountered")
+        
+    createTable("users", "emailID varchar(40), pass varchar(40)")
+    createTable("plantStationData", "location varchar(150), latitudeL double, longitudeL double, sourceType varchar(20), nearestSubstation varchar(150), latitudeSS double, longitudeSS double, plantOwner varchar(150), plantCapacity double")
+    plantDataList = readFile("powerSources")
+    addPlantValues(plantDataList)
+    createTable("demandData", "location varchar(150), latitude double, longitude double, powerDemand double, growthRate float")
+    demandDataList = readFile("demand")
+    addDemandValues(demandDataList)
     return
 
 
@@ -85,9 +113,9 @@ def createTable(tableName, columns):
         tableCursor.execute(f"create table {tableName}({columns});")
         print(tableCursor.fetchall())
         connection.commit()
-        print("Table created")
+        print(f"{tableName} table created")
     if tableError == None:
-        print("Table detected")
+        print(f"{tableName} table detected")
     tableCursor.close()
     return
 
@@ -116,20 +144,26 @@ def addPlantValues(dataList):
         inputCursor.execute(f"insert into plantStationData values(\"{tempList[0]}\", {tempList[1]}, {tempList[2]}, \"{tempList[3]}\", \"{tempList[4]}\", {tempList[5]}, {tempList[6]}, \"{tempList[7]}\", {tempList[8]});")
         connection.commit()
     inputCursor.close()
-    print("Data has been updated")
+    print("plantStationData has been updated")
     return
+
+
+def addDemandValues(dataList):
+    inputCursor = connection.cursor()
+    inputCursor.execute("delete from demandData;")
+    connection.commit()
+    for i in dataList:
+        inputCursor.execute(f"insert into demandData values(\"{i[0]}\", {i[1]}, {i[2]}, {i[3]}, {i[4]})")
+        connection.commit()
+    inputCursor.close()
+    print("demandData has been updated")
+    return
+    
 
 
 def findDistance(x1, y1, x2, y2):
     distance = (((x2 - x1) ** 2) + ((y2 - y1) ** 2)) ** 0.5
     return distance
-
-
-def demandIncrease(monthlyRateOfIncrease, currentDemand):
-    projectedDemand = [currentDemand]
-    for i in range(1, 60):
-        projectedDemand.append(projectedDemand[i - 1] * (1 + (monthlyRateOfIncrease/100)))
-    return projectedDemand
 
 
 def closestSubstation(x, y):
@@ -144,8 +178,29 @@ def closestSubstation(x, y):
     return listSS[distanceSS.index(min(distanceSS))]
 
 
+def demandIncrease(demandLocation):
+    extractionCursor = connection.cursor()
+    extractionCursor.execute(f"select location, powerDemand, growthRate from demandData where location like \"{demandLocation}\";")
+    extracted = extractionCursor.fetchall()[0]
+    currentDemand = extracted[1]
+    rateOfIncrease = extracted[2]
+    projectedDemand = [currentDemand]
+    for i in range(1, 5):
+        projectedDemand.append(projectedDemand[i - 1] * (1 + (rateOfIncrease/100)))
+    return projectedDemand
+
+
+def graphing(demandLoc):
+    yValues = demandIncrease(demandLoc)
+    xpoints = np.array([i for i in range(2023, 2028)])
+    ypoints = np.array([i for i in yValues])
+    plt.plot(xpoints, ypoints)
+    plt.ylabel("Energy consumption in GWh per year")
+    plt.xlabel("Year")
+    plt.title("Projection for increase in power demand using past growth")
+    plt.show()
+    return
+
+
 if __name__ == "__main__":
     init_db()
-    createTable("plantStationData", "location varchar(150), latitudeL double, longitudeL double, sourceType varchar(20), nearestSubstation varchar(150), latitudeSS double, longitudeSS double, plantOwner varchar(150), plantCapacity double")
-    plantDataList = readFile("powerSources")
-    addPlantValues(plantDataList)
